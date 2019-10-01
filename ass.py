e55d44155dbd24e63cfddbb3b24f5c3ebcd501f1
aman17ashray <ashray17aman@gmail.com>
2019-10-01 11:44:40 +0530

    mem access optimized policy

--- ass.py
+++ ass.py
@@ -1,5 +1,85 @@
 import sys
 import math
+
+class TE:
+    valid=False
+    present=False
+    vpn=-1
+    ppn=-1
+    pid=-1
+    def __init__(self,vp,pp,pi):
+        self.vpn=vp
+        self.ppn=pp
+        self.pid=pi
+
+class page:
+    def __init__(self,vpn,pid):
+        self.vpn=vpn
+        self.pid=pid
+
+def allocate(proc_id,time):
+    global ramidx
+    global swapidx
+    alloted[proc_id]=1
+    size=proc_req[proc_id]
+    if size>ram_size:
+        print("size required for process bigger than ram size")
+        return 1
+    npages=int(math.ceil(size/page_size))
+    if proc_id not in page_table.keys():
+        page_table[proc_id]=[TE(i,-1,proc_id) for i in range(npages)]
+    vpn=0
+    if ramidx < ram_size:
+        while ramidx < ram_size and vpn<npages:
+            p=page(vpn,proc_id)
+            ram[ramidx]=[p,time]
+            page_table[proc_id][vpn].ppn=ramidx
+            page_table[proc_id][vpn].present=True
+            page_table[proc_id][vpn].valid=True
+            ramidx+=page_size
+            vpn+=1
+
+    # if vpn<npages:
+    #     while vpn<npages:
+    #         minidx=0          
+    #         for ppn in range(int(ram_size/page_size)):
+    #             if ram[ppn][1]<ram[minidx][1]:
+    #                 minidx=ppn
+           
+                     
+def mem_access(proc_id,query,time,var):
+    if swap_policy=="LRU":
+        print(1)  
+    elif swap_policy=="FIFO":
+        print("hello")
+    elif swap_policy == "Opti":
+        # query is virtual page number
+        # to check if its not present in TLB
+        hit = False
+        for item in tlb:
+            if item[1] == query and item[0] == proc_id:
+                print("TLB HIT")
+                hit = True
+        if not hit:
+            # find the one which'll not be accessed soon
+            # for each element i need to find when do i need this and remove the one needed too later
+            temp = []
+            for item in tlb:
+                # to find when this is needed next
+                flag = -1
+                for que,i in enumerate(proc_queries):
+                    if que == [proc_id,query]:
+                        flag = i
+                        break
+                temp.append(flag)
+                if(flag == -1):
+                    # this is to be removed
+                    
+
+
+
+
+   
 page_size=4
 ram_size=64
 swap_size=128
@@ -11,68 +91,14 @@ alloted=dict()
 page_table=dict()
 ramidx=0
 swapidx=0
-
-class TE:
-    valid=False
-    present=False
-    vpn=-1
-    ppn=-1
-    pid=-1
-   
-    def __init__(self,vp,pp,pi):
-        self.vpn=vp
-        self.ppn=pp
-        self.pid=pi
-
-class page:
-    def __init__(self,vpn,pid):
-        self.vpn=vpn
-        self.pid=pid
        
 tlb=[[TE(-1,-1,-1),-1] for _ in range(tlb_size)]
-ram=[[page(-1,-1),-1] for _ in range(int(ram_size/page_size))]
-swap=[page(-1,-1) for _ in range(int(swap_size/page_size))]
+ram=[[page(-1,-1),-1] for _ in range(int(ram_size))]#changed array size to include each byte
+swap=[page(-1,-1) for _ in range(int(swap_size))]#change array size to include each byte
 
-def allocate(proc_id,time):
-    global ramidx
-    global swapidx
-    alloted[proc_id]=1
-    size=proc_req[proc_id]
-    if size>ram_size:
-        print("size required for process bigger than ram size")
-        return 1
-    npages=math.ceil(size/page_size)
-    if proc_id not in page_table.keys():
-        page_table[proc_id]=[TE(i,-1,proc_id) for i in range(npages)]
-    vpn=0
-    if ramidx < ram_size/page_size:
-        while ramidx < ram_size/page_size and vpn<npages:
-            p=page(vpn,proc_id)
-            ram[ramidx]=[p,time]
-            page_table[proc_id][vpn].ppn=ramidx
-            page_table[proc_id][vpn].present=True
-            page_table[proc_id][vpn].valid=True
-            ramidx+=1
-            vpn+=1
-    if vpn<npages:
-        while vpn<npages:
-            minidx=0
-           
-            for ppn in range(int(ram_size/page_size)):
-                if ram[ppn][1]<ram[minidx][1]:
-                    minidx=ppn
-           
-               
-
-       
-def mem_access(proc_id,query,time,var):
-    if swap_policy=="LRU":
-       
-    elif swap_policy=="FIFO":
-   
      
+   
 
-       
 if __name__=="__main__":
     if page_size<=0:
         print("invalid page size")
@@ -110,8 +136,9 @@ if __name__=="__main__":
             continue
         if alloted[proc_id]==-1:
             error=allocate(proc_id,time)
-        for r in ram:
-            print(r[0].pid, r[1])
+    for r in ram:
+        print(r[0].pid, r[1])
+    print("done")
 #            if not error:
 #                mem_access(proc_id,query,time,True)
 #        else:
